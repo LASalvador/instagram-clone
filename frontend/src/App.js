6ed0d9fcd587c71bc45dba4dc24f64e77440bce4
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-02-24 10:14:24 -0300

    atualizando front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Routes from './routes'
-import { BrowserRouter} from 'react-router-dom';
-import Header from './components/Header/Header';
+import Routes from './route';
+import { BrowserRouter } from 'react-router-dom';
+
+// Components Import
+import Header from './components/header';
 
 function App() {
   return (
-  	<BrowserRouter>
-  		<Header/>
-    	<Routes/>
-  	</BrowserRouter>
-
+    <BrowserRouter>
+      <Header />
+      <Routes />
+    </BrowserRouter>
   );
 }
 
