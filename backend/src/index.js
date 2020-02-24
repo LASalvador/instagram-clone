0c52f1145235fe68a682cbf950469ef2f73a2d13
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-02-24 10:15:29 -0300

    atualizando backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,29 @@
+// require('dotenv').config({ path: __dirname + '/../.env' });
+// console.log(process.env.URL_MONGODB+'b');
+
+const path = require('path')
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
-const routes = require('./routes')
-const app = express();
 
+const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 mongoose.connect('mongodb+srv://root:123@cluster0-rseib.mongodb.net/test?retryWrites=true&w=majority', {
-	useNewUrlParser: true,
-})
-
-var todoSchema = new mongoose.Schema({
-	item:String
+    useNewUrlParser:true,
+    useUnifiedTopology: true 
 });
 
-var Todo = mongoose.model('Todo', todoSchema);
-
-app.use((req, res, next)=> {
-	req.io = io;
-	next();
-})
 app.use(cors());
-app.use(routes);
-app.use('/files', express.static(path.resolve(__dirname, '..','uploads')));
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
 
-server.listen(8081);
+app.use('/files_resize', express.static(path.resolve(__dirname, '..', 'uploads', 'resizes')))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.use(require('./routes'));
+
+server.listen(3333, () => console.log('pai to online'));
